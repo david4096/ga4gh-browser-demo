9d31ca062c116d1818d22704a4d5da189d07c73b
David Steinberg <david@resium.com>
2016-08-02 17:34:02 -0700

    initial

---
+++ demo.sh
@@ -0,0 +1,12 @@
+# -. .-.   .-. .-.   .-. .-.   .
+# ||\|||\ /|||\|||\ /|||\|||\ /|
+# |/ \|||\|||/ \|||\|||/ \|||\||
+# ~   `-~ `-`   `-~ `-`   `-~ `-
+
+# GA4GH Browser Demo 8-2-2016 5:32pm
+
+# I'm running on a mac with homebrew, python, and virtual environment installed.
+
+# First thing would be to clone this repository.
+
+git clone 
